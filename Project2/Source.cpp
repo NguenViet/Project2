935916bb77cef8c27e4536759f25955fa74cc1c0
NguenViet <wiki_croudi@mail.ua>
2019-11-25 12:21:10 +0300

    Добавьте файлы проекта.

---
+++ Project2/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	int y, i, n = 5;
+	printf("input y: ");
+	scanf_s("%d", &y);
+
+	printf("input i: ");
+	scanf_s("%d", &i);
+
+	float Z;
+	int dob = 1;
+	for (int i = 1; i < 6; i++)
+	{
+		dob = cos(dob * i);
+	}
+	Z = dob + 2 * sin(y) / pow(y, 2) + 5;
+	printf("Z=%f\n", Z);
+
+	return 0;
+
+
+
+
+
+
+}
